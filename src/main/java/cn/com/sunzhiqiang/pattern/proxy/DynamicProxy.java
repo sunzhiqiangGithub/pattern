354a72479cdf55db601cded8dd01d94cc1773c81
sunzhiqiang <sunzhiqiang@farmfriend.cn>
2018-08-10 15:28:14 +0800

    动态代理生成代理类的研究

--- src/main/java/cn/com/sunzhiqiang/pattern/proxy/DynamicProxy.java
+++ src/main/java/cn/com/sunzhiqiang/pattern/proxy/DynamicProxy.java
@@ -1,8 +1,14 @@
 package cn.com.sunzhiqiang.pattern.proxy;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Properties;
 
 /**
  * 功能描述: 动态代理
@@ -26,10 +32,20 @@ public class DynamicProxy implements InvocationHandler {
 
     public static void main(String[] args) throws Exception {
 
+        //Properties properties = new Properties();
+        //properties.put("sun.misc.ProxyGenerator.saveGeneratedFiles",true);
+        //System.setProperties(properties);
+
+        //System.out.println(Boolean.parseBoolean(System.getProperty("sun.misc.ProxyGenerator.saveGeneratedFiles")));
+        //byte[] proxyClassByteArray = ProxyGenerator.generateProxyClass("$Proxy1",new Class<?>[]{Phone.class});
+        //BufferedOutputStream buffer = new BufferedOutputStream(new FileOutputStream("D://app/pattern/Proxy2.class"));
+        //buffer.write(proxyClassByteArray);
+        //buffer.close();
+
         DynamicProxy handle = new DynamicProxy(new Boss());
-
         Phone phone = (Phone) Proxy.newProxyInstance(Phone.class.getClassLoader(), new Class[]{Phone.class}, handle);
-
         phone.handlePhone();
+
+        //ProxyGenerator
     }
 }
