2756db7d598e46a216eff769cc24245aefb82363
sunzhiqiangGithub <731265487@qq.com>
2018-09-09 01:32:47 +0800

    Unsafe类初次整理

--- src/main/java/cn/com/sunzhiqiang/java/unsafe/UnsafeStudy.java
+++ src/main/java/cn/com/sunzhiqiang/java/unsafe/UnsafeStudy.java
@@ -18,61 +18,119 @@ public class UnsafeStudy {
 
     class InnerClass {
         private int a = 2;
+        private int b = 4;
     }
 
     public static void main(String[] args) throws Exception {
 
-        UnsafeStudy unsafeStudy = new UnsafeStudy();
+        Unsafe unsafe = getUnsafe();
 
+        //testObjectField(unsafe);
+
+        //testAddress(unsafe);
+
+        //testMemory(unsafe);
+
+        testStaticField(unsafe);
+
+    }
+
+    /**
+     * 测试操作address相关
+     *
+     * @param unsafe
+     */
+    private static void testAddress(Unsafe unsafe) {
+        long memoryAddress = unsafe.allocateMemory(1);
+        System.out.println("分配的内存地址为:" + memoryAddress);
+        unsafe.putAddress(memoryAddress, 2);
+        long address = unsafe.getAddress(memoryAddress);
+        System.out.println("getAddress方法获取的值为:" + address);
+        System.out.println("getByte方法获取的值:" + unsafe.getByte(memoryAddress));
+    }
+
+    /**
+     * 测试获取对象的值和设置对象的值
+     *
+     * @param unsafe
+     * @throws NoSuchFieldException
+     */
+    private static void testObjectField(Unsafe unsafe) throws NoSuchFieldException {
+        InnerClass innerClass = new UnsafeStudy().new InnerClass();
+        int a = unsafe.getInt(innerClass, unsafe.objectFieldOffset(InnerClass.class.getDeclaredField("a")));
+        System.out.println(a);
+        unsafe.putInt(innerClass, unsafe.objectFieldOffset(InnerClass.class.getDeclaredField("a")), 3);
+        System.out.println(innerClass.a);
+    }
+
+    /**
+     * Unsafe类创建对象
+     *
+     * @return
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    private static Unsafe getUnsafe() throws NoSuchFieldException, IllegalAccessException {
         // Unsafe对象的创建
         Field field = Unsafe.class.getDeclaredField("theUnsafe");
         field.setAccessible(true);
         Unsafe unsafe = (Unsafe) field.get(null);
         System.out.println(unsafe);
-
-        // getInt(Object var1, long var2)
-        InnerClass innerClass = new UnsafeStudy().new InnerClass();
-        int a = unsafe.getInt(innerClass, unsafe.objectFieldOffset(InnerClass.class.getDeclaredField("a")));
-        System.out.println(a);
-
-        // putInt(Object var1, long var2, int var4)
-        unsafe.putInt(innerClass, unsafe.objectFieldOffset(InnerClass.class.getDeclaredField("a")), 3);
-        System.out.println(innerClass.a);
-
-        // getInt(long var1) TODO 待研究如何使用
-//        int b = unsafe.getInt(addressOf(unsafe,unsafeStudy));
-//        System.out.println("===" + b);
-
-        // getAddress(long var1) TODO 待研究如何使用
-        long address = unsafe.getAddress(unsafe.allocateMemory(1000));
-        System.out.println(address);
-
-        // allocateMemory(long var1) TODO ?
-        System.out.println(unsafe.allocateMemory(1000));
-
-        // reallocateMemory(long var1, long var3)
-        System.out.println(unsafe.reallocateMemory(unsafe.allocateMemory(1000), 2000));
-
-        //
+        return unsafe;
     }
 
-    public static long addressOf(Unsafe unsafe, Object o) throws Exception {
+    /**
+     * Unsafe类分配内存,设置值,重新分配内存,复制内存释放内存
+     *
+     * @param unsafe
+     * @throws InterruptedException
+     */
+    private static void testMemory(Unsafe unsafe) throws InterruptedException {
+        System.out.println("===========内存分配,设置值,释放内存");
+        long memoryAddress = unsafe.allocateMemory(1);
+        System.out.println("==========第一次的内存地址为:" + memoryAddress);
+        unsafe.setMemory(memoryAddress, 1, (byte) 1);
+        System.out.println("==========第一次内存地址里的数据为:" + unsafe.getByte(memoryAddress));
+        long secondAddress = unsafe.reallocateMemory(memoryAddress, 1);
+        System.out.println("==========第二次的内存地址为:" + secondAddress);
+        unsafe.setMemory(secondAddress, 1, (byte) 2);
+        System.out.println("==========第二次内存地址里的数据为:" + unsafe.getByte(secondAddress));
+        unsafe.freeMemory(memoryAddress);
+        Thread.sleep(1000);
+        unsafe.freeMemory(secondAddress);
+        Thread.sleep(1000);
+        System.out.println(unsafe.getByte(secondAddress));
+        System.out.println("=========测试内存复制");
+        long oneAddress = unsafe.allocateMemory(1);
+        long twoAddress = unsafe.allocateMemory(1);
+        unsafe.setMemory(oneAddress, 1, (byte) 1);
+        System.out.println("被复制的值:" + unsafe.getByte(oneAddress));
+        System.out.println("复制前:" + unsafe.getByte(twoAddress));
+        unsafe.copyMemory(oneAddress, twoAddress, 1);
+        System.out.println("复制后:" + unsafe.getByte(twoAddress));
+    }
 
-        Object[] array = new Object[]{o};
+    /**
+     * 获取和设置静态字段的值
+     *
+     * @param unsafe
+     * @throws NoSuchFieldException
+     */
+    private static void testStaticField(Unsafe unsafe) throws NoSuchFieldException {
+        Object obj = unsafe.staticFieldBase(UnsafeStudy.class.getDeclaredField("c"));
+        long staticFieldOffset = unsafe.staticFieldOffset(UnsafeStudy.class.getDeclaredField("c"));
+        long staticFieldValue = unsafe.getInt(obj, staticFieldOffset);
+        System.out.println(staticFieldValue);
+    }
 
-        long baseOffset = unsafe.arrayBaseOffset(Object[].class);
-        int addressSize = unsafe.addressSize();
-        long objectAddress;
-        switch (addressSize) {
-            case 4:
-                objectAddress = unsafe.getInt(array, baseOffset);
-                break;
-            case 8:
-                objectAddress = unsafe.getLong(array, baseOffset);
-                break;
-            default:
-                throw new Error("unsupported address size: " + addressSize);
-        }
-        return (objectAddress);
+    /**
+     * 使用Unsafe类创建对象
+     *
+     * @param unsafe
+     * @throws InstantiationException
+     */
+    private static void testCreateInstance(Unsafe unsafe) throws InstantiationException {
+        UnsafeStudy unsafeStudy = UnsafeStudy.class.cast(unsafe.allocateInstance(UnsafeStudy.class));
+        System.out.println(unsafeStudy.b);
     }
 }
