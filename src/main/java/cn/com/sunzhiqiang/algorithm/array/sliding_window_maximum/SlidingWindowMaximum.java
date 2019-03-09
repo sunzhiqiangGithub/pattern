5bc66d32f97cf4cc6657b9503205cb9e10e02c7f
sunzhiqiangGithub <731265487@qq.com>
2019-03-09 23:37:56 +0800

    滑动窗口解法2

--- src/main/java/cn/com/sunzhiqiang/algorithm/array/sliding_window_maximum/SlidingWindowMaximum.java
+++ src/main/java/cn/com/sunzhiqiang/algorithm/array/sliding_window_maximum/SlidingWindowMaximum.java
@@ -15,6 +15,9 @@ public class SlidingWindowMaximum {
 
         int[] result = maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
         System.out.println(Arrays.toString(result));
+
+        result = maxSlidingWindow2(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
+        System.out.println(Arrays.toString(result));
     }
 
     public static int[] maxSlidingWindow(int[] nums, int k) {
@@ -42,5 +45,42 @@ public class SlidingWindowMaximum {
         }
 
         return result;
+    }
+
+    public static int[] maxSlidingWindow2(int[] nums, int k) {
+
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+
+        int maxIndex = getMaxIndex(nums, 0, k - 1);
+        int[] result = new int[nums.length - k + 1];
+        result[0] = nums[maxIndex];
+
+        for (int i = 1, j = k; i < result.length; i++, j++) {
+            if (maxIndex >= i) {
+                if (nums[j] >= nums[maxIndex]) {
+                    maxIndex = j;
+                }
+            } else {
+                maxIndex = getMaxIndex(nums, i, j);
+            }
+
+            result[i] = nums[maxIndex];
+        }
+
+        return result;
+    }
+
+    private static int getMaxIndex(int[] nums, int start, int end) {
+
+        int maxIndex = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (nums[i] >= nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
     }
 }
